11f7e8e4948567989836b4bdf9e90f558a53acb8
みちのく61 <michinoku61@gmail.com>
2019-11-17 20:58:45 +0900

    imageカラムの削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_17_042930) do
+ActiveRecord::Schema.define(version: 2019_11_17_115551) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.text "text"
-    t.text "image"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
